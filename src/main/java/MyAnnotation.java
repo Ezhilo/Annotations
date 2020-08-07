313997b3e509e24ce1f13cf0b497f67eb7e6c4de
ezhilo <ezhilo@mail.ru>
2020-08-07 19:11:33 +0300

    Final

--- src/main/java/MyAnnotation.java
+++ src/main/java/MyAnnotation.java
@@ -4,4 +4,8 @@ import java.lang.annotation.RetentionPolicy;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface MyAnnotation {
 
+    String name();
+    boolean boolParam() default false;
+
+
 }
--- src/main/java/Sample.java
+++ src/main/java/Sample.java
@@ -1,16 +1,35 @@
-import jdk.nashorn.internal.objects.annotations.Getter;
-
 import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 import java.util.Arrays;
-@MyAnnotation
+@MyAnnotation(name = "Name1")
 public class Sample {
 
     public static void main(String[] args) {
 
-        Sample sample = new Sample();
-        Class aClass = sample.getClass();
+        Operations(Sample.class);
+
+    }
+
+      private static void Operations(Class aClass) {
+
+        Annotation annotation = aClass.getAnnotation(MyAnnotation.class);
+        MyAnnotation annotation1 = (MyAnnotation) annotation;
+        String name = annotation1.name();
+        System.out.println(name);
+
         Annotation[] annotations = aClass.getAnnotations();
+        Arrays.stream(annotations).forEach(System.out::println);
+
         System.out.println(Arrays.toString(annotations));
+
+        Method[] declaredMethods = aClass.getDeclaredMethods();
+        Arrays.stream(declaredMethods).forEach(System.out::println);
+
+
+    }
+
+    @MyMethodAnnotation
+    public void simpleMethod(){
 
     }
 }
